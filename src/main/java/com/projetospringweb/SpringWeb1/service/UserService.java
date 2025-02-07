5fe1102dbbb2386a45dc93bf5dc9dfbfe9499526
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2025-02-07 04:14:10 -0300

    Adiciona anotação @Service para registrar o componente UserService corretamente no Spring.

--- src/main/java/com/projetospringweb/SpringWeb1/service/UserService.java
+++ src/main/java/com/projetospringweb/SpringWeb1/service/UserService.java
@@ -3,10 +3,13 @@ package com.projetospringweb.SpringWeb1.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.projetospringweb.SpringWeb1.entities.User;
 import com.projetospringweb.SpringWeb1.repositories.UserRepository;
 
+
+@Service
 public class UserService {
 
 	@Autowired
