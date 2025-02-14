72c9454c0efdfcbd860411248ab58899857615b4
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2025-02-14 12:46:11 -0300

    Adicionada a classe 'Order' com relacionamentos e chave estrangeira com a classe 'User'.

--- src/main/java/com/projetospringweb/SpringWeb1/entities/User.java
+++ src/main/java/com/projetospringweb/SpringWeb1/entities/User.java
@@ -1,12 +1,15 @@
 package com.projetospringweb.SpringWeb1.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	// Define um relacionamento um-para-muitos com a entidade Order
+	// Um usuário (client) pode ter vários pedidos associados a ele
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 
@@ -73,6 +80,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
